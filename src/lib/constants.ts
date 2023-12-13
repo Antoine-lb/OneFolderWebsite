e7db357780319573658669ea484bb0763fb7b07e
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2023-12-13 19:29:45 +0100

    footer done

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,3 +2,4 @@ export const CTA_URL = "https://forms.gle/UPmNKp9FncKYnvcW7";
 export const YOUTUBE_URL = "https://youtu.be/5VEm7qhPo2M";
 export const CALENDLY_URL =
   "https://calendly.com/photofolder/photofolder-feedback";
+export const GITHUB_LINK = "https://github.com/OneFolderApp/OneFolder";
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { page } from "$app/stores";
-  import { CTA_URL } from "$lib/constants";
+  import { CTA_URL, GITHUB_LINK } from "$lib/constants";
   import "../app.css";
 
   export let data;
@@ -19,9 +19,11 @@
 <footer class="flex text-white justify-center bg-black py-14">
   <div class="max-w-2xl text-center">
     <h3 class="text-4xl font-extrabold">
-      Built for the <span class="italic">long term</span>
+      Built for the <br class="block md:hidden" /><span class="italic"
+        >long term</span
+      >
     </h3>
-    <p class="text-lg mt-3">
+    <p class="text-lg mt-3 p-4">
       All software comes to an end, it's inevitable. But files have to last
       forever. That's why our top priority is to create OneFolder so that you
       can access your photos after OneFolder has disappeared.
@@ -35,15 +37,52 @@
     >
       Get early access 🌼
     </a>
-    <!-- <div class="flex justify-center flex-col">
-    <a href="/blog" class=" !text-[#005DC4]"> Blog </a>
-    more from our blog:
-    {#each data.posts as post}
-      <a href={`/blog/${post.slug}`} class=" !text-[#005DC4]">
-        {post.title}
-      </a>
-    {/each}
-  </div> -->
+    <div class="flex text-left flex-col md:flex-row gap-8 justify-center mt-16">
+      <div class="flex flex-col justify-start text-left">
+        {#each data.posts as post}
+          <a href={`/blog/${post.slug}`} class=" !text-[#fff]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="inline ml-2 fill-white -rotate-45"
+              height="1em"
+              width="1em"
+              viewBox="0 0 448 512"
+              ><path
+                d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+              /></svg
+            >
+            {post.title}
+          </a>
+        {/each}
+        <a href="/blog" class=" !text-[#fff]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline ml-2 fill-white -rotate-45"
+            height="1em"
+            width="1em"
+            viewBox="0 0 448 512"
+            ><path
+              d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+            /></svg
+          > All articles from our blog
+        </a>
+      </div>
+      <div>
+        <a href={GITHUB_LINK} class=" !text-[#fff]">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="inline ml-2 fill-white -rotate-45"
+            height="1em"
+            width="1em"
+            viewBox="0 0 448 512"
+            ><path
+              d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"
+            /></svg
+          >
+          Github
+        </a>
+      </div>
+    </div>
   </div>
   <br />
   <br />
