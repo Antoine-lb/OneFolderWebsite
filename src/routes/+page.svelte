3676e8a281467ac5f147bbca64349ea51ec785b9
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2023-12-19 15:34:29 +0100

    new screenshots

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -205,7 +205,7 @@
 <!-- FILE -->
 <!-- FILE -->
 <!-- FILE -->
-<!-- <div
+<div
   class="flex flex-col max-w-4xl mt-5 m-auto justify-center items-center text-center mb-16"
 >
   <h2
@@ -232,12 +232,44 @@
       class="  m-auto w-full max-w-[700px]"
     />
   </div>
-</div> -->
+</div>
+
+<!-- TAGS -->
+<!-- TAGS -->
+<!-- TAGS -->
+<div
+  class="flex flex-col max-w-4xl mt-5 m-auto justify-center items-center text-center mb-16"
+>
+  <h2
+    class="text-5xl drop-shadow-lg px-4 font-bold text-[#005cc4] fill-[#458cdd] flex justify-center"
+  >
+    Tags <svg
+      class="inline ml-2"
+      xmlns="http://www.w3.org/2000/svg"
+      height="1em"
+      viewBox="0 0 512 512"
+    >
+      <path
+        d="M64 480H448c35.3 0 64-28.7 64-64V160c0-35.3-28.7-64-64-64H288c-10.1 0-19.6-4.7-25.6-12.8L243.2 57.6C231.1 41.5 212.1 32 192 32H64C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64z"
+      />
+    </svg>
+  </h2>
+  <p class="text-xl md:text-2xl text-[#444] mt-1 mb-4">
+    Browse your files the way you are used to
+  </p>
+  <div class="px-4">
+    <img
+      src="screens2/tags-screen.webp"
+      alt="OneFolder tags screen"
+      class="  m-auto w-full max-w-[700px]"
+    />
+  </div>
+</div>
 
 <!-- CALENDAR -->
 <!-- CALENDAR -->
 <!-- CALENDAR -->
-<!-- <div
+<div
   class="flex flex-col max-w-4xl mt-5 m-auto justify-center items-center text-center mb-16"
 >
   <h2
@@ -258,17 +290,17 @@
   <p class="text-xl md:text-2xl text-[#444] mt-1 mb-4">Same as on your phone</p>
   <div class="px-4">
     <img
-      src="/calendar.webp"
-      alt="OneFolder logo"
+      src="screens2/calendar-screen.webp"
+      alt="OneFolder calendar screenshot"
       class="  m-auto w-full max-w-[700px]"
     />
   </div>
-</div> -->
+</div>
 
 <!-- MAP -->
 <!-- MAP -->
 <!-- MAP -->
-<!-- <div
+<div
   class="flex flex-col max-w-4xl mt-5 m-auto justify-center items-center text-center mb-16"
 >
   <h2
@@ -289,17 +321,17 @@
   <p class="text-xl md:text-2xl text-[#444] mt-1 mb-4">Search geographically</p>
   <div class="px-4">
     <img
-      src="/map.webp"
-      alt="OneFolder logo"
+      src="screens2/map-screen.webp"
+      alt="OneFolder map screenshot"
       class="  m-auto w-full max-w-[700px]"
     />
   </div>
-</div> -->
+</div>
 
 <!-- FACES -->
 <!-- FACES -->
 <!-- FACES -->
-<!-- <div
+<div
   class="flex flex-col max-w-4xl mt-5 m-auto justify-center items-center text-center mb-16"
 >
   <h2
@@ -322,12 +354,12 @@
   </p>
   <div class="px-4">
     <img
-      src="/faces.webp"
-      alt="OneFolder logo"
+      src="screens2/faces-screen.webp"
+      alt="OneFolder faces screenshot"
       class="  m-auto w-full max-w-[700px]"
     />
   </div>
-</div> -->
+</div>
 
 <h3
   class="text-4xl mt-12 drop-shadow-lg text-[#333] px-4 font-bold mb-4 text-center"
