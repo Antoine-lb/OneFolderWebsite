f7811b0531ccdbf232a4ef6b7b7f75355689627c
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2024-01-19 17:50:45 +0100

    dowload page not done

--- src/routes/download/+page.svelte
+++ src/routes/download/+page.svelte
@@ -5,33 +5,13 @@
   const REPO_LINK_API =
     "https://api.github.com/repos/PhotoFolder/OneFolder/releases/latest";
 
-  let windowsDownloadLink: HTMLElement | null;
-  let macDownloadLink: HTMLElement | null;
-  let linuxDownloadLink: HTMLElement | null;
+  let windowsLink = REPO_LINK;
+  let macLink = REPO_LINK;
+  let linuxLink = REPO_LINK;
 
   onMount(() => {
-    windowsDownloadLink = document.getElementById("download-windows");
-    macDownloadLink = document.getElementById("download-mac");
-    linuxDownloadLink = document.getElementById("download-linux");
-    highlightOSButton();
     setDownloadLinks();
   });
-  // Highlight download button for user's platform
-  function highlightOSButton() {
-    var ua = navigator.userAgent;
-    var link;
-    if (ua.indexOf("Win") !== -1) {
-      link = windowsDownloadLink;
-    } else if (ua.indexOf("Mac") !== -1) {
-      link = macDownloadLink;
-    } else if (ua.indexOf("Linux") !== -1) {
-      link = linuxDownloadLink;
-    }
-    if (link) {
-      link.classList.remove("alt");
-      link.classList.add("strong");
-    }
-  }
 
   async function setDownloadLinks() {
     const response = await fetch(REPO_LINK_API, {
@@ -46,93 +26,70 @@
       const asset = assets[i];
       if (
         asset.content_type.startsWith("application") &&
-        asset.name.startsWith("OneFolder-") &&
-        windowsDownloadLink &&
-        macDownloadLink &&
-        linuxDownloadLink
+        asset.name.startsWith("OneFolder-")
       ) {
         if (asset.name.endsWith(".exe")) {
-          windowsDownloadLink.href = asset.browser_download_url;
+          windowsLink = asset.browser_download_url;
         } else if (asset.name.endsWith(".dmg")) {
-          macDownloadLink.href = asset.browser_download_url;
+          macLink = asset.browser_download_url;
         } else if (asset.name.endsWith(".AppImage")) {
-          linuxDownloadLink.href = asset.browser_download_url;
+          linuxLink = asset.browser_download_url;
         }
       }
     }
   }
 </script>
 
-<div class="  flex flex-col justify-between">
-  <div class="px-4">
-    <!-- <img src="/landing/hero-image.webp" class=" mt-10 m-auto" alt="example of a plate" /> -->
-    <img
-      src="/hero-image-7.webp"
-      alt="OneFolder logo"
-      class="-mt-[1.5rem] md:-mt-8 m-auto w-[600px]"
-    />
+<div class="px-4">
+  <img
+    src="/hero-image-7.webp"
+    alt="OneFolder logo"
+    class="-mt-[1.5rem] md:-mt-[18rem] m-auto w-[600px]"
+  />
+</div>
+<h1
+  class="text-center title-font text-4xl md:text-7xl drop-shadow-lg px-4 font-bold text-[#333]"
+>
+  OneFolder
+</h1>
+<h2 class="text-center font-bold text-2xl mt-12 mb-3 text-[#333]">
+  Dowload Page
+</h2>
+
+<div class="flex flex-col items-center gap-5 max-w-xs w-full m-auto">
+  <div class="bg-[#FDDEDE] text-center p-2 rounded">
+    <p class="font-bold">Warning before using OneFolder</p>
+    <p class="font-light">
+      No matter how battle tested a software can be, always have backups.
+    </p>
   </div>
-  <h1 class="font-bold text-[#444] text-4xl text-center my-8">
-    OneFolder <span
-      class="font-bold text-[#e89b03] fill-[#fcb322] border-[#fcb322ca] px-3 pb-1 rounded-full inline-flex items-center border-2"
-    >
-      Beta
 
-      <svg
-        class="inline ml-2 h-8"
-        xmlns="http://www.w3.org/2000/svg"
-        height="1em"
-        viewBox="0 0 448 512"
-        ><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path
-          d="M288 0H160 128C110.3 0 96 14.3 96 32s14.3 32 32 32V196.8c0 11.8-3.3 23.5-9.5 33.5L10.3 406.2C3.6 417.2 0 429.7 0 442.6C0 480.9 31.1 512 69.4 512H378.6c38.3 0 69.4-31.1 69.4-69.4c0-12.8-3.6-25.4-10.3-36.4L329.5 230.4c-6.2-10.1-9.5-21.7-9.5-33.5V64c17.7 0 32-14.3 32-32s-14.3-32-32-32H288zM192 196.8V64h64V196.8c0 23.7 6.6 46.9 19 67.1L309.5 320h-171L173 263.9c12.4-20.2 19-43.4 19-67.1z"
-        /></svg
-      >
-    </span>
-  </h1>
+  <div class="h-[1.5px] bg-[#bbb] w-full" />
 
-  <div class="flex flex-col items-center mt-12 gap-3">
-    <a
-      id="download-windows"
-      href={REPO_LINK}
-      class="inline-flex text-[#f3f3ec] p-2 px-3 hover:px-4 transition-all rounded-xl text-2xl items-center shadow-xl bg-[#FF5543]"
-    >
-      Download for Windows
-    </a>
-    <a
-      class="inline-flex text-[#f3f3ec] p-2 px-3 hover:px-4 transition-all rounded-xl text-2xl items-center shadow-xl bg-[#FF5543]"
-      id="download-mac"
-      href={REPO_LINK}
-    >
-      Download for Mac
-    </a>
-    <p class="text-[#777] -mt-2 italic">intel/silicon</p>
-    <p class="text-[#555555] mt-7 italic">comming very very soon:</p>
+  <a
+    id="download-windows"
+    href={windowsLink}
+    class="cta-button w-full flex justify-center !text-white !no-underline px-3 py-3 text-xl hover:bg-[#ff5643e2] transition-all"
+  >
+    <img src="/windows.svg" alt="OneFolder logo" class="mr-1" />
+    Download for Windows
+  </a>
+  <a
+    class="cta-button w-full flex justify-center !text-white !no-underline px-3 py-3 text-xl hover:bg-[#ff5643e2] transition-all"
+    id="download-mac"
+    href={macLink}
+  >
+    <img src="/mac.svg" alt="OneFolder logo" class="mr-1" />
 
-    <a
-      id="download-linux"
-      href={REPO_LINK}
-      class="hidden bg-[#fff] !text-[#646464] border-2 border-[#7f7f7f] border-dashed p-2 px-3 rounded-xl text-2xl items-center cursor-progress"
-    >
-      Download for Linux
-    </a>
+    Download for Mac
+  </a>
 
-    <a
-      id="download-linux"
-      class="inline-flex bg-[#fff] !text-[#646464] border-2 border-[#7f7f7f] border-dashed p-2 px-3 rounded-xl text-2xl items-center cursor-progress"
-    >
-      Download for Linux
-    </a>
+  <div class="h-[1.5px] bg-[#bbb] w-full" />
 
-    <p class="text-[#555555] mt-8 italic">comming not so soon:</p>
-    <div
-      class="inline-flex bg-[#fff] text-[#646464] border-2 border-[#7f7f7f] border-dashed p-2 px-3 rounded-xl text-2xl items-center cursor-progress"
-    >
-      iOS App
-    </div>
-    <div
-      class="inline-flex bg-[#fff] text-[#646464] border-2 border-[#7f7f7f] border-dashed p-2 px-3 rounded-xl text-2xl items-center cursor-progress"
-    >
-      Android App
-    </div>
-  </div>
+  <a
+    id="download-linux"
+    class="cta-button-off w-full flex justify-center !no-underline px-3 py-3 text-xl hover:bg-[#ff5643e2] transition-all"
+  >
+    Get notified when Linux is ready
+  </a>
 </div>
