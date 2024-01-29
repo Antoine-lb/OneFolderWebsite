32413a92ee71faa37ee1398211c5cd4422ba8285
Antoine <27826950+Antoine-lb@users.noreply.github.com>
2024-01-29 19:15:07 +0100

    gettings started not done

---
+++ src/routes/getting-started/+page.svelte
@@ -0,0 +1,34 @@
+<header class="fixed flex justify-between w-full p-3">
+  <a class=" flex items-center gap-2 !no-underline" href="/">
+    <img src="/favicon.svg" class="h-8" alt="a small flower" />
+    <span class="text-2xl text-[#333] font-light">OneFolder</span>
+  </a>
+</header>
+
+<br />
+<br />
+<br />
+<br />
+
+<div class="video-wrapper">
+  <iframe
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/ESCBe3pBPfI?si=5exH0kpJuJC-_XZX"
+    title="YouTube video player"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+    allowfullscreen
+  />
+</div>
+
+<style>
+  .video-wrapper {
+    aspect-ratio: 16 / 9;
+  }
+
+  .video-wrapper iframe {
+    width: 100%;
+    height: 100%;
+  }
+</style>
